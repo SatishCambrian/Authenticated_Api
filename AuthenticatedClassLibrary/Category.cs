9e5a4be58649e3b593b61778527921d2113cfbee
SatishCambrian <SatishCambrian@users.noreply.github.com>
2024-02-22 23:01:53 -0500

    Created a new Category model that has an Id and description. Added a Category property to the Product model.

---
+++ AuthenticatedClassLibrary/Category.cs
@@ -0,0 +1,5 @@
+namespace AuthenticatedClassLibrary;
+public class Category{
+    public int Id { get; set; };
+    public string Description { get; set; };
+}
--- AuthenticatedClassLibrary/Product.cs
+++ AuthenticatedClassLibrary/Product.cs
@@ -7,5 +7,5 @@ public class Product
     public string Name { get; set; }
     public string Description { get; set; }
 
-    // public Category  Category { get; set; }
+    public Category  Category { get; set; }
 }
